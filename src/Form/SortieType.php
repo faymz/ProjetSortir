a794c5ccb20ca799489d03b9a4b7beb54f7e58cc
faymz <joakim.k@hotmail.fr>
2022-10-18 10:57:51 +0200

    ajout des crud pour participant, sortie, lieu, campus, ville, etat

---
+++ src/Form/SortieType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sortie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('dateHeureDebut')
+            ->add('duree')
+            ->add('dateLimiteInscription')
+            ->add('nbInscriptionsMax')
+            ->add('infosSortie')
+            ->add('motifAnnulation')
+            ->add('participants')
+            ->add('organisateur')
+            ->add('siteOrganisateur')
+            ->add('etat')
+            ->add('lieu')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
