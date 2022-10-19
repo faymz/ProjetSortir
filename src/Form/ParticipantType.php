b298be759d44ef5bab534134616f5cc473a74092
faymz <joakim.k@hotmail.fr>
2022-10-19 11:42:33 +0200

    ajout modification de profile

--- src/Form/ParticipantType.php
+++ src/Form/ParticipantType.php
@@ -8,9 +8,13 @@ use App\Repository\CampusRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ParticipantType extends AbstractType
@@ -18,21 +22,55 @@ class ParticipantType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('nom')
+            ->add('pseudo')
             ->add('prenom')
-            ->add('telephone')
-            ->add('actif')
-            ->add('administrateur')
+            ->add('nom')
+            ->add('telephone', telType::class)
+            ->add('email')
+            //->add('roles')
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les champs de mot de passe doivent correspondre.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmation'],
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmation'],
+            ])
+            ->add('campus', EntityType::class, [
+                'class' => Campus::class,
+                'label' => "Campus :",
+                'placeholder' => '--- Sélectionner ---',
+                'choice_label' => 'nom',
+                'query_builder' => function (CampusRepository $campusRepository){
+                    return $campusRepository->createQueryBuilder('c')->orderBy('c.nom', 'ASC');
+                },
+                'constraints' => new NotBlank()
+
+            ])
+            //->add('actif')
+            //->add('administrateur')
             ->add('image', fileType::class, [
                 'mapped' => false,
                 'constraints' => [ new Image( ["mimeTypesMessage" => "Le format de fichier n'est pas autorisé."])
                 ]
             ])
-            ->add('campus')
-            ->add('inscrit')
+
+            //->add('inscrit')
         ;
     }
 
